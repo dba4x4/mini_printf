71340246e5cbf0fb8f8f04b57378752943563042
piranucc <piranucc@student.42.fr>
2016-12-15 15:15:42 +0100

    very small version

--- ft_printf.c
+++ ft_printf.c
@@ -6,61 +6,60 @@
 /*   By: piranucc <piranucc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/15 13:48:07 by piranucc          #+#    #+#             */
-/*   Updated: 2016/12/15 14:47:57 by piranucc         ###   ########.fr       */
+/*   Updated: 2016/12/15 15:15:14 by piranucc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
 # include <stdarg.h>
 # include <stdint.h>
-
-
 #include <stdio.h>
-int		ft_printf(int fd, char *str, ...)
-{
-	va_list pa;
-	int ret;
-	int i;
-	char *s;
 
-	i = 0;
+int		print_s(char *s, int fd)
+{
+	int ret;
+
+	write(fd, s, ft_strlen(s));
+	ret = ft_strlen(s);
+	return (ret);
+}
+
+int		print_i(int lennb, int fd)
+{
+
+	int ret;
+
+	ret = ft_len_nb(lennb);
+	ft_putnbr_fd(lennb, fd);
+	return (ret);
+
+}
+
+int		fd_printf(int fd, char *str, ...)
+{
+	va_list	pa;
+	int		ret;
+	int		i;
+
+	i = -1;
 	va_start(pa, str);
 	ret = 0;
-	while (str[i] !='\0')
+	while (str[++i] !='\0')
 	{
 		if (str[i] == '%')
 		{
 			i++;
 			if (str[i] == 's')
-			{
-				s = va_arg(pa, char *);
-				write(1, s, ft_strlen(s));
-				//i++;
-				va_end(pa);
-			}
+				ret += print_s(va_arg(pa, char *), fd);
 			else if (str[i] == 'i')
-			{
-				ft_putnbr(va_arg(pa, int));
-				//i++;
-			}
+				ret += print_i(va_arg(pa, int), fd);
 		}
 		else
+		{
 			ft_putchar_fd(str[i], fd);
-		i++;
-
+			ret ++;
+		}
 	}
 	va_end(pa);
 	return (ret);
-}
-
-
-int main()
-{
-	int i;
-
-	i = 300;
-	ft_printf(1,"debut =>%s | suite => %i | fin =>%s", "FIRST", i, "LAST");
-	//ft_printf(1,"coucou %i tout va bien", i);
-	return (0);
-
 }
